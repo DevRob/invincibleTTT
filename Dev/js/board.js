8779b16cf094bbd5f314f14142d83010d4345fc4
Robert Molnar <mail.robertmolnar@gmail.com>
2016-01-19 08:43:28 +0000

    update: .js refactored

--- Dev/js/board.js
+++ Dev/js/board.js
@@ -1,151 +1,102 @@
-/**
- * Board constructor to represent the gameboard as an array of arrays. Can be used
- *    for other games, size adjustable.
- */
+var BOARDSIZE = 3;
 
-var Board = function(dim, squares) {
- /**
-  * Create Board class.
-  */
- this.dim = dim;
- if (!squares) {
-   this.init();
+var Board = function(squares) {
+ if (squares) {
+   this.squares = squares.slice();
  } else {
-   this.squares = getSquares(squares);
+   this.squares = generateArray(Math.pow(BOARDSIZE, 2), null);
  }
 };
 
-Board.prototype.init = function () {
-  /**
-   * Initialise board.
-   */
-  this.squares = allItemMatrix(null, this.dim, this.dim);
-};
+Board.prototype.move = function(square, player) {
+  this.squares[square] = player;
+}
 
 Board.prototype.getEmptySquares = function() {
-  /**
-   * Get the [row, col] index pair of empty squares.
-   */
-  var squares = this.squares,
-      emptySquares = [];
-
-  for (var row = 0, numRows = squares.length; row < numRows; row++) {
-    for (var col = 0, numCol = squares[row].length; col < numCol; col++) {
-      if (squares[row][col] === null) {
-        emptySquares.push([row, col]);
-      }
+  var emptySquares = [];
+  this.squares.forEach(function(square, index) {
+    if (square == null) {
+      emptySquares.push(index);
     }
-  }
+  });
   return emptySquares;
-};
+}
 
-Board.prototype.move = function(pos, player) {
-  /**
-   * Place player's piece on board.
-   */
-  var row = pos[0],
-      col = pos[1];
-
-  if (this.squares[row][col] === null) {
-    this.squares[row][col] = player;
-  }
-};
+Board.prototype.getSquare = function(index) {
+  return this.squares[index];
+}
 
 Board.prototype.checkWin = function() {
-  /**
-   * Check winning conditions.
-   */
-  self = this;
-  self.lines = [];
-  self.winnerLine = null;
-  self.gameStatus = null;
-  /**
-  * Get rows by make a copy of the board since the board is
-  *    represented as an array of rows of the board.
-  */
-  self.lines.extend(self.squares);
-  /**
-  * Get columns iterating through the rows and picking the
-  *    corresponding element for the column.
-  */
-  var cols = [];
-  for (var i = 0, len = self.dim; i < len; i++) {
-    cols.push([]);
-    self.squares.forEach(function(row) {
-      cols[i].push(row[i]);
-    });
-  }
-  self.lines.extend(cols);
-  /**
-  * Get diagonal lines
-  */
-  var diag1 = [],
-      diag2 = [];
-  for (var row = 0, numRows = self.squares.length; row < numRows; row++) {
-    diag1.push(self.squares[row][row]);
-    diag2.push(self.squares[row][Math.abs(row - self.squares[row].length) - 1]);
-  }
-
-  self.lines.push(diag1);
-  self.lines.push(diag2);
-  /**
-  * Iterate through the possible winning combinations and check
-  *    if they match winning conditions.
-  */
-  for (var idx = 0, lines = self.lines, leng = self.lines.length; idx < leng; idx++) {
-    var lineSet = new Set(lines[idx]);
+  var winnerLine = {player: null, line: null};
+  var lines = this.getWinnerLines();
+  for (var index = 0, len = lines.length; index < len; index++) {
+    var lineSet = new Set(lines[index]);
     if (lineSet.size === 1) { // Set.size = 1 ->> elements of line are the same.
-      if (!lineSet.has(null)) {
-        self.winnerLine = self.lines.indexOf(lines[idx]);
-        if (lines[idx][0] === 0) {
-          self.gameStatus = 1;
-          break;
-        } else {
-          self.gameStatus = 2;
-          break;
-        }
+      winnerLine.player = lineSet.values().next().value;
+      if (winnerLine.player !== null) {
+        winnerLine.line = index;
+        break;
       }
-    } else if (self.getEmptySquares().length === 0) {
-      self.gameStatus = 3;
+    } else if (this.isFull()){
+      winnerLine.player = TIE;
     }
   }
-  return self.gameStatus;
-};
+  return winnerLine;
+}
 
-Board.prototype.logBoard = function() {
-  /**
-  * log the board on the console for debugging purposes if needed.
-  *    I had a great use of it.
-  */
+Board.prototype.getWinnerLines = function() {
+  var lines = [];
+  lines.extend = function(array) {
+    this.push.apply(this, array);
+    return this;
+  }
+  lines.extend(this.getRows()).extend(this.getCols()).extend(this.getDiagonals());
+  return lines;
+}
+
+Board.prototype.getRows = function() {
+  var rows = [[],[],[]];
+  this.squares.forEach(function(square, index) {
+    rows[parseInt(index / BOARDSIZE)].push(square);
+  });
+  return rows;
+}
+
+Board.prototype.getCols = function() {
+  var cols = [[],[],[]];
+  this.squares.forEach(function(square, index) {
+    cols[parseInt(index % BOARDSIZE)].push(square);
+  });
+  return cols;
+}
+
+Board.prototype.getDiagonals = function() {
   var squares = this.squares;
-  var cells = [];
-  console.log("-------------------");
-  squares.forEach(function(row) {
-    row.forEach(function(cell) {
-      if (cell === 0) {
-        cells.push("O");
-      } else if (cell == 1) {
-        cells.push("X");
-      } else {
-        cells.push("-");
-      }
-    });
-    console.log(cells);
-    cells = [];
-  });
-  console.log("-------------------");
-};
+  var diagonals = [[],[]];
+  for (i = 0; i < BOARDSIZE; i++) {
+    diagonals[0].push(squares[i * BOARDSIZE + i]);
+    diagonals[1].push(squares[(i + 1) * (BOARDSIZE - 1)]);
+  }
+  return diagonals;
+}
 
-function getSquares(squares) {
-  /*
-  * Helper function to clone the board.
-  */
-  var squaresClone = [];
-  squares.forEach(function(row) {
-    squaresClone.push([]);
-    row.forEach(function(cell) {
-      squaresClone[squares.indexOf(row)].push(cell);
-    });
-  });
-  return squaresClone;
+Board.prototype.isEmpty = function () {
+  if (this.getEmptySquares().length == Math.pow(BOARDSIZE, 2)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+Board.prototype.isFull = function () {
+  if (this.getEmptySquares().length == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function generateArray(length, element) {
+  var arr = Array.apply(null, Array(length));
+  return arr.map(function() {return element;});
 }
--- Dev/js/game.js
+++ Dev/js/game.js
@@ -1,165 +1,60 @@
-/**
- * Game and Player constructor.
- */
-var Player = function(player) {
-  /**
-   * Storing player info.
-   */
-  this.playerName = pickName(player[1]);
-  this.playerType = player[0];
+var PLAYERO = 0;
+var PLAYERX = 1;
+var TIE = 2;
+var NAMES = ["bob", "tom", "selly", "katy", "zorg", "daro", "rik", "nisi"];
 
+var Player = function(player) {
+  /** @param {Object} player - name and type. */
+  this.playerName = pickName(player.name);
+  this.playerType = player.type;
   function pickName(name) {
-    // Return a rndom name from the list if player name not specified.
     if (!name) {
-      return Math.randomChoice(["bob", "tom", "selly", "katy", "zorg"]);
+      return randomChoice(NAMES);
     }
     return name;
   }
 };
 
 var Game = function(settings) {
-  /**
-   * This contains game settings and tracking the game state,
-   *    plus drawing board and moves on canvas.
-   */
-
-  this.boardSize = settings.boardSize;
-  this.board = new Board(this.boardSize);
-  this.squareSize = canvas.width / this.boardSize;
-  this.players = [new Player(settings.player1), new Player(settings.player2)];
-  this.winner = null;
-  this.frameSetup();
-  this.firstMove = settings.firstMove;
-  this.actualPlayer = this.setFirstPlayer(this.firstMove);
+  /** @param {Object} settings - game settings from DOM elements. */
+  this.board = new Board();
+  this.currentPlayer = this.setFirstPlayer(settings.firstMove);
+  this.players = {
+    0: new Player(settings.player1),
+    1: new Player(settings.player2)
+  };
 };
 
-Game.prototype.setFirstPlayer = function() {
-  /**
-   * Set first player.
-   */
-  if (this.firstMove > 1) {
-    return  Math.randomChoice([0, 1]);
+Game.prototype.setFirstPlayer = function(firstMove) {
+  if (firstMove > 1) {
+    return randomChoice([PLAYERO, PLAYERX]);
   } else {
-    return parseInt(this.firstMove);
+    return parseInt(firstMove);
   }
 };
 
-Game.prototype.frameSetup = function() {
-  /**
-   * Draw the grid on canvas.
-   */
-  for (var i = 1, len = this.boardSize; i < len; i++) {
-    lineStart = [this.squareSize * i, 0];
-    lineEnd = [this.squareSize * i, canvas.height];
-    for (var j = 0; j < 2; j++) {
-      ctx.beginPath();
-      ctx.moveTo(lineStart[0], lineStart[1]);
-      ctx.lineTo(lineEnd[0], lineEnd[1]);
-      ctx.lineWidth = 5;
-      ctx.strokeStyle = '#8894a0';
-      ctx.stroke();
-      lineStart.reverse();
-      lineEnd.reverse();
-    }
+Game.prototype.switchPlayer = function() {
+ this.currentPlayer = nextPlayer(this.currentPlayer);
+}
+
+Game.prototype.inProgress = function() {
+  var winner = this.board.checkWin().player;
+  if (winner === null) {
+    return true;
+  } else {
+    return false
   }
-};
-
-Game.prototype.drawMove = function () {
-  /**
-   * Draws the player's move on the board.
-   */
-  var squares = this.board.squares;
-  var squareSize = this.squareSize;
-
-  for (var row = 0, rowLen = squares.length; row < rowLen; row++) {
-    for (var col = 0, colLen = squares[row].length; col < colLen; col++) {
-        if (squares[row][col] === 0) {
-          ctx.beginPath();
-          ctx.arc(this.getCoord(col), this.getCoord(row), canvas.width / (2 * this.boardSize) * 0.5, 0, 2 * Math.PI);
-          ctx.strokeStyle = '#626e7a';
-          ctx.lineWidth = this.squareSize / 10;
-          ctx.stroke();
-        } else if (squares[row][col] === 1) {
-          ctx.beginPath();
-          ctx.moveTo(this.getCoord(col) + squareSize / 4, this.getCoord(row) + squareSize / 4);
-          ctx.lineTo(this.getCoord(col) - squareSize / 4, this.getCoord(row) - squareSize / 4);
-          ctx.moveTo(this.getCoord(col) + squareSize / 4, this.getCoord(row) - squareSize / 4);
-          ctx.lineTo(this.getCoord(col) - squareSize / 4, this.getCoord(row) + squareSize / 4);
-          ctx.strokeStyle = '#27a8e0';
-          ctx.lineCap="round";
-          ctx.lineWidth = this.squareSize / 10;
-          ctx.stroke();
-        }
-      }
-    }
- };
-
- Game.prototype.drawWin = function() {
-   /**
-    * Draw winner cross-out line if there is a winner.
-    */
-   var squares = this.board.squares;
-   var squareSize = this.squareSize;
-   var lines = this.board.lines;
-   var dim = parseInt(this.board.dim);
-   var winLineIdx = parseInt(this.board.winnerLine);
-   var endpoints = getEndPoints(winLineIdx);
-   var player = this.actualPlayer;
-
-   ctx.beginPath();
-   /**
-    * Offset the endpoint of the cross-out with 12 pixel so it's
-    *    not perfectly alligned with the lines of the X sign.
-    */
-   ctx.moveTo(this.getCoord(endpoints[0][1]), this.getCoord(endpoints[0][0]) + 12);
-   ctx.lineTo(this.getCoord(endpoints[1][1]), this.getCoord(endpoints[1][0]) - 12);
-   ctx.strokeStyle = '#8894a0';
-   ctx.lineCap="round";
-   ctx.lineWidth = this.squareSize / 7;
-   ctx.stroke();
-
-   function getEndPoints(winLineIdx) {
-     // Get the coordinates of the winner-line's endpoints.
-     var calc = {
-       0: [[winLineIdx, 0],[winLineIdx, dim-1]],
-       1: [[0, winLineIdx.mod(dim)],[dim-1, winLineIdx.mod(dim)]],
-       2: [[(winLineIdx-2*dim)*(dim-1), 0],[((winLineIdx-2*dim+1).mod(2))*(dim-1), dim-1]]
-     };
-     return calc[parseInt(winLineIdx / dim)];
-   }
- };
-
- Game.prototype.getCoord = function(idx) {
-   /**
-    * Given a grid position in the form (row or col), returns
-    *    the coordinates on the canvas of the center of the grid.
-    */
-   return this.squareSize * (idx + 0.5);
- };
-
- Game.prototype.getSquare = function(x, y) {
-   /**
-    * Given coordinates on a canvas, gets the indices of the grid.
-    */
-    return {row: parseInt(y / this.squareSize),
-            col: parseInt(x / this.squareSize)};
- };
+}
 
  Game.prototype.resetGame = function() {
-   /**
-    * Reset game.
-    */
-   this.board.init();
-   ctx.clearRect(0, 0, canvas.width, canvas.height);
-   this.setFirstPlayer();
-   this.frameSetup();
-   this.winner = null;
-
+   this.board = new Board();
+   this.switchPlayer();
  };
 
- function switchPlayer(actualPlayer) {
-   /**
-    * Convenience function to switch players. Returns other player.
-    */
-   return parseInt((actualPlayer + 1) % 2);
+ Game.prototype.getPlayerType = function() {
+   return this.players[this.currentPlayer].playerType;
  }
+
+ function randomChoice(array) {
+   return array[parseInt(Math.floor(Math.random() * array.length))];
+ };
